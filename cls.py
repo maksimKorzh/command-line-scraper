e8a65ce52d8566a538708b144821d9a5007fa7f7
Maksim Korzh <freesoft.for.people@gmail.com>
2019-11-14 10:58:26 +0200

    addesd json config file fed support

--- cls.py
+++ cls.py
@@ -1,129 +1,99 @@
-###############################################################
+#######################################################################
 #
 # Libraries
 #
-###############################################################
+#######################################################################
 
 import requests
-from bs4 import BeautifulSoup
-import sys
+import tabulate
 import json
 import csv
-import tabulate
+import sys
+from bs4 import BeautifulSoup
 
 
-###############################################################
+#######################################################################
 #
 # Scraper
 #
-###############################################################
+#######################################################################
 
-def scrape(url):
-    # Try to get the given URL
-    try:
-        html = requests.get(url).text
-               
-    # Quit program otherwise
-    except:
-        print('Failed to retrieve url "%s"' % url)
-        sys.exit()
+def scrape(file_name):
+    # Raw string format configuration data
+    config_file = ''
+    
+    # Open configuration file
+    with open(file_name, 'r') as json_file:
+        for row in json_file.read():
+            config_file += row
+    
+    # Dictionary structured configuration data
+    config_file = json.loads(config_file)
+    
+    # Make HTTP GET request to fetch text data from URL
+    html = requests.get(config_file['url'])
     
     # Parse content
-    content = BeautifulSoup(html, 'lxml')
+    content = BeautifulSoup(html.text, 'lxml')
     
-    # Parse selectors
-    try:
-        # Init content selectors
-        selectors = json.loads(sys.argv[2])
-        
-        # Create variable to store results
-        results = []
-        
-        # Create item to store extracted data
-        item = {}
-        
-        # Loop over selectors
-        for tag in selectors['tags']:
-            # Append selector class to item
-            item[tag['class']] = [one.text for one in content.findAll(tag['tag'], {'class': tag['class']})]
-        
-        # loop over row indexes
-        for index in range(0, len(item[next(iter(item))])):
-            # Create single 'row' to append to table
-            row = {}
-            
-            # loop over stored data
-            for key, value in item.items():
-                # append value to the new row
-                row[key] = '"' + value[index] + '"'
-            
-            # append new row to results list
-            results.append(row)
-        
-        # Specify table field names
-        field_names = []
-
-        # Loop over selectors
-        for selector in selectors['tags']:
-            # Loop over selector's items
-            for key, val in selector.items():
-                # Use class names as field names
-                if key == 'class':
-                    field_names.append(val)
-        
-        # Print table to console
-        print(tabulate.tabulate([row.values() for row in results], field_names, tablefmt="fancy_grid"))
-            
-        # Try to write results to CSV file
-        try:
-            # Store file name
-            file_name = sys.argv[3]
-
-            # Write results to CSV file
-            with open(file_name, 'w', newline='') as file_csv:
-                # Init dictionary writer
-                writer = csv.DictWriter(file_csv, fieldnames=field_names)
-                
-                # Write table header
-                writer.writeheader()
-                
-                # Populate table with rows
-                for row in results:
-                    writer.writerow(row)
-        
-        # Except the case if file name is not provided
-        except:
-            print('Provide "filename.csv" as the last argument to store file')
+    # Create variable to store results
+    results = []
     
-    # Except malformed json string
-    except:
-        print('Failed to parse selectors!')
+    # Init content selectors
+    tags = config_file['tags']
+    
+    # Create item to store extracted data
+    scraped_items = {}
+    
+    # Loop over selectors
+    for tag in tags:
+        scraped_items[tag['class']] = [tag.text for tag in content.findAll(tag['tag'], {"class": tag['class']})]
+    
+    # Loop over the list of scraped items by tag
+    for index in range(0, len(scraped_items[next(iter(scraped_items))])):
+        # Create variable to store result row
+        row = {}
+        
+        # Group scraped items' keys and values
+        for key, val in scraped_items.items():
+            row[key] = '"' + val[index] + '"'
+        
+        # Append the row to results list
+        results.append(row)
 
+    # Print table to console
+    print(tabulate.tabulate([row.values() for row in results], config_file['columns'], tablefmt="fancy_grid"))
 
-###############################################################
+    # Write results to CSV file
+    with open(config_file['output'], 'w', newline='') as file_csv:
+        # Init dictionary writer
+        writer = csv.DictWriter(file_csv, fieldnames=config_file['columns'])
+
+        # Write table header
+        writer.writeheader()
+
+        # Populate table with rows
+        for row in results:
+            writer.writerow(row)
+
+#######################################################################
 #
 # Main
 #
-###############################################################
+#######################################################################
 
-# If no command line options specified
-if len(sys.argv) == 1:
-    print('Type "cls.py -help" to get some help...')
-    sys.exit()
+# Check the config file availability
+if sys.argv[1]:
+    scrape(sys.argv[1])
 
-# Print HELP ,essage
-elif sys.argv[1] == '-help':
-    print('Usage: ...')
-
-# Run scraper
-elif 'http' in sys.argv[1]:
-    # URL to scrape data from
-    url = sys.argv[1]
+# Exit the program if no config file provided
+else:
+    print('Example usage: cls.py your_config_file.json')
     
-    # Run scraper
-    scrape(url)
     
-
-
     
-
+    
+    
+    
+    
+    
